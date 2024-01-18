f382db34f7d2a36cc841d01b48392110262e7677
BastienVienet <bastien.vienet@tipee.ch>
2024-01-18 08:40:12 +0100

    B2: KISS: On partait dans quelque chose de trop compliqué. C'est bien plus simple comme ça.

--- src/main.rs
+++ src/main.rs
@@ -33,11 +33,8 @@ fn divide(num1: i32, num2: i32) -> i32 {
     num1 / num2
 }
 
-fn modulo(num1: i32, _num2: i32) -> i32 {
-    if num1 == 23 {
-        return 3;
-    }
-    else { return 0 };
+fn modulo(num1: i32, num2: i32) -> i32 {
+    num1 % num2
 }
 
 fn main() {
