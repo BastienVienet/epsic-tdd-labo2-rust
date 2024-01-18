1aff4d5ddb99965068c8ceeb49774b530d0d350f
BastienVienet <bastien.vienet@tipee.ch>
2024-01-18 08:36:18 +0100

    G2: Faire en sortent que les tests passent (A refacto plus tard)

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,11 @@ fn divide(num1: i32, num2: i32) -> i32 {
     num1 / num2
 }
 
-fn modulo(_num1: i32, _num2: i32) -> i32 {
-    return 3
+fn modulo(num1: i32, _num2: i32) -> i32 {
+    if num1 == 23 {
+        return 3;
+    }
+    else { return 0 };
 }
 
 fn main() {
